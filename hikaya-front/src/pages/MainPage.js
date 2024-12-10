42d960098bb1f3ced8f9c824d9d786ba066cc4b2
EsraaEid2 <esraa.eidd2@gmail.com>
2024-12-10 06:57:41 +0300

    pull

--- hikaya-front/src/pages/MainPage.js
+++ hikaya-front/src/pages/MainPage.js
@@ -1,13 +1,29 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import Swal from "sweetalert2"; // Ensure Swal is imported
 import Sidebar from "../components/MainPage/Sidebar";
 import RightSidebar from "../components/MainPage/RightSidebar";
 import "../components/MainPage/MainPage.css";
-
 import Search from "../components/landing_page/SearchComponent";
-import WriteStory from "../components/story_components/writeStory"; // Add your login component import
+import WriteStory from "../components/story_components/writeStory";
 
 function MainPage() {
+  const navigate = useNavigate(); // Initialize navigate
   const [isExploreClicked, setIsExploreClicked] = useState(true); // Default to true to show Explore
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (!token) {
+      Swal.fire({
+        icon: "info",
+        title: "Login Required",
+        text: "Please log in to write a story.",
+        confirmButtonText: "Go to Login",
+      }).then(() => {
+        navigate("/login"); // Navigate to login after Swal confirmation
+      });
+    }
+  }, [navigate]);
 
   const handleExploreClick = () => {
     setIsExploreClicked(true);
@@ -23,18 +39,14 @@ function MainPage() {
         <section id="header" className="jumbotron text-center">
           <h1 className="display-3 logo-text">Hikaya</h1>
           <p className="lead">Discover inspiring stories from people around the world</p>
-          <a href="#" className="submitButton" onClick={handleLoginClick}>
+          <button className="submitButton" onClick={handleLoginClick}>
             Start Sharing
-          </a>
-          <a
-            href="#"
-            className="submitButton"
-            onClick={handleExploreClick}
-          >
+          </button>
+          <button className="submitButton" onClick={handleExploreClick}>
             Explore Stories
-          </a>
+          </button>
         </section>
-        {isExploreClicked ? <Search /> : <WriteStory />}{" "}
+        {isExploreClicked ? <Search /> : <WriteStory />}
         {/* Conditional rendering */}
       </div>
     </div>
